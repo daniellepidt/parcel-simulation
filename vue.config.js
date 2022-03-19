e559e7c7e53667c32fa7e4afe63509d349f173bf
Daniel Pidtylok <101867533+daniellepidt@users.noreply.github.com>
2022-03-19 11:16:20 +0200

    Add files via upload

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  transpileDependencies: [
+    'vuetify'
+  ],
+
+  pwa: {
+    name: 'Menny\'s Parcel',
+    themeColor: '#344955',
+    msTileColor: '#F9AA33',
+    manifestOptions: {
+      background_color: '#F9AA33'
+    }
+  }
+}
